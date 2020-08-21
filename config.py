bdba8c025e94147d1c432fa29385bad085588bf7
Kirill Povstaynoy <rywes@yandex.ru>
2020-08-22 01:09:30 +0300

    waether bot

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-token = ""
-api_key = ""
+token = "1366720944:AAGOMT9U2yH8U6-FTME__liny5SSpU_J9BA"
+api_key = "694e9f98c5bb2106e1ca790afafb4314"
 url = "https://api.openweathermap.org/data/2.5/weather?"
